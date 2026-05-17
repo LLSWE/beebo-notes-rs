4e50c9206929a298e6bd1cb5745d0ca2238b1b52
LLSWE <tiilas777@gmail.com>
2026-05-17 13:05:12 -0300

    updt ignore

--- beebo_api/src/model/server.rs
+++ beebo_api/src/model/server.rs
@@ -2,12 +2,14 @@ use std::env;
 
 pub struct HttpServer {
     pub port: String,
+    pub api_key: String,
 }
 
 impl HttpServer {
     pub fn from_env() -> Self {
         Self {
             port: env::var("AXUM_API_PORT").expect("[ERROR] Missing port"),
+            api_key: env::var("AXUM_API_KEY").expect("[ERROR] Missing api key"),
         }
     }
 }
